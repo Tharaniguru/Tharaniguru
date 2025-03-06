c87766a78a2d05910bb46df60487702e7faa8c62
Tharaniguru <tharaniguru0110@gmail.com>
2025-03-06 14:24:12 +0530

    firstpage

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,26 +6,26 @@ function Header() {
 
   return (
     <header className='flex justify-between px-5 bg-blue-900 py-5  text-white font-semibold'>
-        <a className='font-extrabold font-serif' href="#"> PORTFOLIO</a>
+        <a className='font-extrabold font-serif' href="/#"> PORTFOLIO</a>
         <nav className='hidden md:block'>
             <ul className='flex'>
-            <li><a href="#"> Home</a> </li>
-            <li> <a href="#"> About</a></li>
-            <li> <a href="#"> Projects</a></li>
-            <li> <a href="#"> Contact</a></li>
+            <li><a href="/#"> Home</a> </li>
+            <li> <a href="/#"> About</a></li>
+            <li> <a href="/#"> Projects</a></li>
+            <li> <a href="/#"> Contact</a></li>
         </ul>
         </nav>
 
         { toggleMenu && <nav className='block md:hidden '>
             <ul className='flex flex-col p-5 mobile-nav'>
-            <li><a href="#"> Home</a> </li>
-            <li> <a href="#"> About</a></li>
-            <li> <a href="#"> Projects</a></li>
-            <li> <a href="#"> Contact</a></li>
+            <li><a href="/#"> Home</a> </li>
+            <li> <a href="/#"> About</a></li>
+            <li> <a href="/#"> Projects</a></li>
+            <li> <a href="/#"> Contact</a></li>
         </ul>
         </nav>}
 
-        <button onClick={()=>setToggleMenu(!toggleMenu)} className='block md:hidden'>
+        <button onClick={()=>setToggleMenu(!toggleMenu)} className='block md:hidden my-2'>
             <Bars3Icon className='text-white h-5'/>
         </button>
         
